b9520bbf0ba8a817aad78455c65589d84d764db7
baranyildirim <baran.yildirim@outlook.com>
2018-02-17 11:26:22 -0800

    Set Map to zoom in on UCLA Campus by default.

--- app/src/main/java/com/hoth/bruinmap/MapsActivity.java
+++ app/src/main/java/com/hoth/bruinmap/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +38,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng start = new LatLng(34.0689, -118.4452);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                new CameraPosition.Builder()
+                .target(start)
+                .tilt(0)
+                .zoom(15)
+                .bearing(90)
+                .build()));
     }
 }
